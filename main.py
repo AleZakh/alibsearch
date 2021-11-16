8b204d0a27f41568533aaf64eb09558b29bf4f15
Pavel Stroganov <pdizreq@gmail.com>
2021-11-16 22:19:27 +0300

    prototype rate limiter

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import re
 from collections import namedtuple
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from typing import Optional, Generator
+from time import sleep
 
 import requests
 import requests.adapters
@@ -20,9 +21,10 @@ MAX_PARALLEL = 500
 ISBN_REGEX = re.compile(r'\(ISBN: (.*?)\)')  # ISBN number
 PRICE_REGEX = re.compile(r'Цена: (.*) руб.')  # price
 URL_FILTER_REGEX = re.compile('find3')
+DELAY_REGEX = re.compile(r'Продолжить работу можно через (\d+) секунд')
 
 
-def alib(url: str, query: str) -> Generator[Book]:  # parsing the 1st or/and next pages
+def alib(url: str, query: str) -> Generator[Book, None, None]:  # parsing the 1st or/and next pages
     with requests.Session() as ses:
         ses.mount('http://', requests.adapters.HTTPAdapter(pool_maxsize=MAX_PARALLEL))
         ses.mount('https://', requests.adapters.HTTPAdapter(pool_maxsize=MAX_PARALLEL))
@@ -30,8 +32,8 @@ def alib(url: str, query: str) -> Generator[Book]:  # parsing the 1st or/and nex
         yield from get_books(res, ses=ses)
 
 
-def get_books(res: requests.Response, ses: Optional[requests.Session] = None) -> Generator[Book]:
-    logging.info(res.url)
+def get_books(res: requests.Response, ses: Optional[requests.Session] = None) -> Generator[Book, None, None]:
+    logging.info(f'{res.url}\n{res.text[:200]}')
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
     yield from search_page(soup)
@@ -40,15 +42,27 @@ def get_books(res: requests.Response, ses: Optional[requests.Session] = None) ->
         pages_links = soup.find_all('a', href=URL_FILTER_REGEX)
         if len(pages_links) > 1:
             with ThreadPoolExecutor(min(len(pages_links), MAX_PARALLEL)) as ex:
-                futures = (ex.submit(ses.get, f'https:{page["href"]}') for page in pages_links)
+                futures = (ex.submit(get_page, url=f'https:{page["href"]}', ses=ses) for page in pages_links)
                 for future in as_completed(futures):
                     yield from get_books(res=future.result())
         elif pages_links:
-            res = ses.get(f'https:{pages_links[0]["href"]}')
-            yield from get_books(res=res)
+            for page in pages_links:
+                res = get_page(url=f'https:{page["href"]}', ses=ses)
+                yield from get_books(res=res)
 
 
-def search_page(soup: bs4.BeautifulSoup) -> Generator[Book]:  # parsing one webpage to list
+def get_page(url: str, ses: requests.Session, params: Optional[dict] = None) -> requests.Response:
+    res = ses.get(url=url, params=params)
+
+    delay_search = DELAY_REGEX.search(res.text)
+    if delay_search:
+        sleep(int(delay_search.group(1)) + 1)
+        get_page(url=url, ses=ses, params=params)
+
+    return res
+
+
+def search_page(soup: bs4.BeautifulSoup) -> Generator[Book, None, None]:  # parsing one webpage to list
     ent: bs4.Tag
     for ent in soup.select(f'body > p:has(a[href*="bs.php"])'):
         name = re.sub(r'\s+', ' ', ent.b.text.strip())  # book name extraction and cleaning
