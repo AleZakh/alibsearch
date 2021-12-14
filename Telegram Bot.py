98931853b464d6b94ee8417d5473de5337559a97
Alezakh <unhead21@gmail.com>
2021-12-14 18:12:15 +0100

    added Telegram bot module and connect it with main

--- Telegram Bot.py
+++ Telegram Bot.py
@@ -1,16 +1,22 @@
 # !/usr/bin/env python3
 import main
 import telebot
+from telebot import types
 
-bot = telebot.TeleBot('5016356128:AAFbLEBBdyT2Y1Lfx5nNnJJiR8GuusWGW5I', parse_mode=None)
+with open('bot_token.txt') as t:
+    token = t.read()
+
+bot = telebot.TeleBot(token, parse_mode=None)
 user_dict = {}
+
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
+    markup = types.ReplyKeyboardRemove(selective=False)
     msg = bot.reply_to(message, """\
 Hi!
 Input author or/and book name in russian:
-""")
+""", reply_markup=markup)
     bot.register_next_step_handler(msg, name_step)
 
 
@@ -21,7 +27,8 @@ def name_step(message):
         bot.register_next_step_handler(msg, price_step)
     except:
         bot.reply_to(message, "oooops, something went wrong. Let's try again")
-    #    send_welcome('start')
+        restart(message.chat.id)
+
 
 def price_step(message):
     try:
@@ -31,20 +38,58 @@ def price_step(message):
             return
         price = int(message.text)
         user_dict[message.chat.id].append(price)
-        print (user_dict[message.chat.id])
+        print(user_dict[message.chat.id])
         result = main.main(user_dict[message.chat.id][0])
-        for i in result:
-            if i[2] < price:
-                bot.send_message(message.chat.id, f'{i[0]}, price {i[2]} rub, link: {i[3]}')
+
+        if not result:
+            bot.send_message(message.chat.id, f'Nothing was found on alib.ru. Try another author or/and book name')
+        else:
+            minprice = main.minprice(result)
+            user_dict[message.chat.id].append(minprice)
+            if minprice > price:
+                msg = bot.send_message(message.chat.id, f'''
+                Nothing was found in your price range (<{price} руб)
+                Minimal price is {minprice}.
+                Show minimal price positions?
+                ''', reply_markup=yes_no_markup())
+                bot.register_next_step_handler(msg, lambda m: altprice_step(m, result, minprice))
+            else:
+                show_result(message.chat.id, result, price)
+
     except:
         bot.reply_to(message, "oooops, something went wrong. Let's try again")
-    #    send_welcome('start')
+        restart(message.chat.id)
+
+
+def altprice_step(message, result, minprice):
+    if message.text == 'yes':
+        show_result(message.chat.id, result, minprice)
+    else:
+        restart(message.chat.id)
+
+
+def show_result(chat_id, result, price):
+    for i in result:
+        if i[2] <= price:
+            bot.send_message(chat_id, f'{i[0]}, price {i[2]} rub, link: {i[3]}')
+    restart(chat_id)
 
 
 bot.enable_save_next_step_handlers(delay=2)
-
-# Load next_step_handlers from save file (default "./.handlers-saves/step.save")
-# WARNING It will work only if enable_save_next_step_handlers was called!
 bot.load_next_step_handlers()
+
+
+def restart(chat_id):
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    markup.add(types.KeyboardButton('/start'))
+    bot.send_message(chat_id, "Restart?", reply_markup=markup)
+
+
+def yes_no_markup():
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    markup.add(types.KeyboardButton('yes'))
+    markup.add(types.KeyboardButton('no'))
+    return markup
+
 
 bot.infinity_polling()
