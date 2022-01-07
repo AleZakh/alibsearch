2f303e35c745182dd4014b9a7ae57080f5d44793
Alezakh <unhead21@gmail.com>
2022-01-07 18:23:18 +0100

    added watchlist_search thread

--- tbot_inline.py
+++ tbot_inline.py
@@ -7,17 +7,19 @@ import telebot
 from telebot import types
 import csv
 import logging
+from threading import Thread
+import schedule
+import time
 
 # import sys
-# import schedule
-# import time
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 user_dict = {}
 user_result = []
+
 with open('bot_token.txt') as t:
     token = t.read()
-    bot = telebot.TeleBot(token, parse_mode=None)
+bot = telebot.TeleBot(token, parse_mode=None)
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -30,11 +32,11 @@ def callback_query(call):
     if call.data == 'clear':
         clear_watchlist(call.from_user.id)
     if call.data == 'yes':
-        show_result(0,call.from_user.id)
+        show_result(0, call.from_user.id)
     if call.data == 'return':
         return_to_start(call.from_user.id)
     try:
-        show_result(int(call.data),call.from_user.id)
+        show_result(int(call.data), call.from_user.id)
     except Exception as e:
         logging.info(e)
 
@@ -42,13 +44,14 @@ def callback_query(call):
 @bot.message_handler(func=lambda message: True)
 def send_welcome(message):
     user_dict[message.chat.id] = {}
-    user_dict[message.chat.id]['chat_id']=message.chat.id
+    user_dict[message.chat.id]['chat_id'] = message.chat.id
     logging.info(user_dict)
-    msg=bot.reply_to(message, """\
+    msg = bot.reply_to(message, """\
 Hi!
 Do you want to search for books or add one to a watchlist?
 """, reply_markup=search_add_markup())
     user_dict[message.chat.id]['1st_message_id'] = msg.message_id
+
 
 def first_question(call):
     user_dict[call.from_user.id]['call'] = call.data
@@ -57,6 +60,7 @@ def first_question(call):
         msg = bot.send_message(user_dict[call.from_user.id]['chat_id'], 'Input author or/and book name:')
         user_dict[call.from_user.id]['last_message_id'] = msg.message_id
         bot.register_next_step_handler(msg, price_step)
+
 
 def price_step(message):
     try:
@@ -136,10 +140,10 @@ def search_result(msg):
             user_result.extend(list(filter(lambda c: c[:][2] <= user_dict[msg.chat.id]['price'], user_list)))
             user_dict[msg.chat.id]['result_pages'] = len(user_result) // 5 if len(user_result) % 5 == 0 \
                 else len(user_result) // 5 + 1
-        show_result(0,msg.chat.id)
+        show_result(0, msg.chat.id)
 
 
-def show_result(page_number,chat_id):
+def show_result(page_number, chat_id):
     logging.info(page_number)
     result_message_text = ''
     i = page_number * 5
@@ -155,13 +159,13 @@ def show_result(page_number,chat_id):
                               chat_id=chat_id,
                               parse_mode='MarkdownV2',
                               disable_web_page_preview=True,
-                              reply_markup=result_markup(page_number,chat_id))
+                              reply_markup=result_markup(page_number, chat_id))
     else:
         result_message = bot.send_message(chat_id,
                                           text=result_message_text,
                                           parse_mode='MarkdownV2',
                                           disable_web_page_preview=True,
-                                          reply_markup=result_markup(page_number,chat_id))
+                                          reply_markup=result_markup(page_number, chat_id))
         user_dict[chat_id]['last_message_id'] = result_message.message_id
         user_dict[chat_id]['is_result_msg'] = True
         logging.info(user_dict)
@@ -208,7 +212,7 @@ def clear_watchlist(chat_id):
 
 
 def return_to_start(chat_id):
-    i = user_dict[chat_id]['1st_message_id']+1
+    i = user_dict[chat_id]['1st_message_id'] + 1
     while i <= user_dict[chat_id]['last_message_id'] + 1:
         try:
             bot.delete_message(chat_id=user_dict[chat_id]['chat_id'], message_id=i)
@@ -244,7 +248,7 @@ def watchlist_markup():
     return markup
 
 
-def result_markup(cur_page,chat_id):
+def result_markup(cur_page, chat_id):
     logging.info(str(cur_page) + ' of ' + str(user_dict[chat_id]['result_pages'] - 1))
     markup = types.InlineKeyboardMarkup(row_width=2)
     markup.row(types.InlineKeyboardButton('1<<', callback_data='0') if cur_page >= 2
@@ -276,5 +280,38 @@ def telegram_parser_format(txt):
         txt = txt.replace(i, '\\' + i)
     return txt
 
-
 bot.infinity_polling()
+
+def watchlist_search():
+    with open('watchlist.csv', newline='', encoding='utf-8') as wl:
+        reader = csv.reader(wl)
+        watchlist = list(reader)
+        logging.info(watchlist)
+    try:
+        for row in watchlist:
+            user_list = (list(filter(lambda c: c[:][2] <= watchlist, alib_search.main(row[1]))))
+            logging.info(user_list)
+            if len(user_list) > 0:
+                msg = bot.send_message(row[0], 'Your watchlist query was found!')
+                user_dict[row[0]]['last_message_id'] = msg.message_id
+                user_dict[row[0]]['1st_message_id'] = msg.message_id
+                user_result.extend(user_list)
+                user_dict[row[0]]['result_pages'] = len(user_result) // 5 if len(user_result) % 5 == 0 \
+                    else len(user_result) // 5 + 1
+                show_result(0, row[0])
+
+
+    except Exception as e:
+        logging.info(e)
+
+def schedule_checker():
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
+
+if __name__ == "__main__":
+    schedule.every().day.at("22:00").do(watchlist_search())
+    schedule.every().day.at("7:00").do(watchlist_search())
+    Thread(target=schedule_checker).start()
+
+    #server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
