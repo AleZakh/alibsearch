1600ac5b10e12fd603e8e0d7b6e6b83ed86e9352
Alezakh <unhead21@gmail.com>
2022-01-18 12:35:17 +0100

    fixes on add_to_watchlist

--- tbot_inline.py
+++ tbot_inline.py
@@ -111,9 +111,8 @@ def result_step(message):
 
 def add_to_watchlist(msg):
 
-    r.append(user_dict[msg.chat.id]["chat_id"],
-            [user_dict[msg.chat.id]["query"],
-            user_dict[msg.chat.id]["price"]])
+    r.append(user_dict[msg.chat.id]["chat_id"],user_dict[msg.chat.id]["query"])
+    r.append(user_dict[msg.chat.id]["chat_id"],user_dict[msg.chat.id]["price"])
     msg = bot.send_message(user_dict[msg.chat.id]["chat_id"], f''' 
             ✍🏻 {user_dict[msg.chat.id]["query"]} by less then {user_dict[msg.chat.id]["price"]} rub added to watchlist
             - Database updates every 09:00 and 23:00 (GMT+3)
